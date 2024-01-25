dfcdd672aa006116b1581de3e1ddb4d259802422
hari saputra <hari.saputra.dev@gmail.com>
2024-01-25 10:01:46 +0700

    add details page with dynamic route

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Housinglocation } from '../housinglocation';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   template: `
     <section class="listing">
       <img
@@ -16,6 +17,7 @@ import { Housinglocation } from '../housinglocation';
       <p class="listing-location">
         {{ housingLocation.city }}, {{ housingLocation.state }}
       </p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrl: './housing-location.component.css',
